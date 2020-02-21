86ef65f613ee618a85c20f233727dc2fbbfcd24e
陈志培 <chenzhipei@hotoa.com>
2020-02-21 16:04:52 +0800

    监听message设置邮件收件人

--- data/conf/sogo/custom-sogo.js
+++ data/conf/sogo/custom-sogo.js
@@ -1,7 +1,20 @@
 // Custom SOGo JS
 
 // Change the visible font-size in the editor, this does not change the font of a html message by default
-CKEDITOR.addCss("body {font-size: 16px !important}");
+// CKEDITOR.addCss("body {font-size: 16px !important}");
 
 // Enable scayt by default
 //CKEDITOR.config.scayt_autoStartup = true;
+
+window.addEventListener('message',function(e){
+    var data = e.data;
+    if(data && data.tag_element && data.field){
+        var input = angular.element(document.getElementById(data.tag_element));
+        if(data.selection){
+            data.selection.forEach(function(item){
+                input.controller().addRecipient(item.name + "   <" + item.email + ">;", data.field);
+            })
+        }
+        input.controller().contactFilter();
+    }
+},false);
