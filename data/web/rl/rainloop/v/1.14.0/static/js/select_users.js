14e803e3334c45dc999b0a9089d3a6eccb71fd51
root <root@localhost.localdomain>
2020-12-21 18:49:08 +0800

    rainloop集成集团OA通讯录

--- data/web/rl/rainloop/v/1.14.0/static/js/select_users.js
+++ data/web/rl/rainloop/v/1.14.0/static/js/select_users.js
@@ -1,5 +1,3 @@
-
-console.log("select_users----------------");
 //设置语言为简体中文
 // CKEDITOR.lang.detect('zh-cn');
 
@@ -16,58 +14,44 @@ function addMessageListener(){
     console.log("addBodyListeneraddBodyListener");
     window.addEventListener('message',function(e){
         var data = e.data;
-        var key = 'mi_' + Math.random().toString( 16 ).slice( 2, 10 );
-        var obj = {
-            dkimStatus: "none",
-            dkimValue: "",
-            email: "test2@steedos.cn",
-            name: ""
-        }
-        var self = this;
-        // self._createTag(obj.email, key, obj);
-        console.log("data--------: ",data);
-        console.log("ddddddddd: ",__webpack_require__);
+        var type = ".e-row";
         if(data && data.tag_element && data.field){
-            var input = angular.element(document.getElementsByClassName(data.tag_element)[0]);
-            console.log("document--------: ",document.getElementsByClassName(data.tag_element)[0]);
-            console.log("input--------: ",input);
+            type = "." + data.field + "-row";
             if(data.selection){
-                data.selection.forEach(function(item){
-                    input.controller().addRecipient(item.name + "   <" + item.email + ">;", data.field);
-                })
+                if (data.field == "to"){
+                    data.selection.forEach(function(item){
+                        $("#rainloop_mailEditor").closest(".e-row").find(".e-value .inputosaurus-input .ui-autocomplete-input").val(item.email).blur();
+                    })
+                }else{
+                    data.selection.forEach(function(item){
+                        $(type).find(".e-value .inputosaurus-input .ui-autocomplete-input").val(item.email).blur();
+                    })
+                }
             }
-            input.controller().contactFilter();
         }
     },false);
 };
 
 //给收件人添加样式，让其看起来像A链接
-// function addStyles(){
-//     console.log("addStylesaddStyles");
-//     var style = document.createElement("style");
-//     var str = "#rainloop_mailEditor {color:#336699;text-decoration:underline; padding-bottom:3px;box-shadow:none;cursor:pointer}";
-//     style.type="text/css"; 
-//     if(style.styleSheet){
-//         style.styleSheet.cssText = str;  
-//     } else {  
-//         style.innerHTML = str; 
-//     }  
-//     document.getElementsByTagName("head")[0].appendChild(style);  
-// };
+function addStyles(){
+    var style = document.createElement("style");
+    var str = "#rainloop_mailEditor {color:#336699;text-decoration:underline; padding-bottom:3px;box-shadow:none;cursor:pointer}";
+    style.type="text/css"; 
+    if(style.styleSheet){
+        style.styleSheet.cssText = str;  
+    } else {  
+        style.innerHTML = str; 
+    }  
+    document.getElementsByTagName("head")[0].appendChild(style);  
+};
 
 //给body添加click监听，如果点击的是to,cc,bcc的label，就会弹出组织选人界面
 function addBodyListener(){
-    console.log("addBodyListeneraddBodyListener: ", document.body);
     document.body.addEventListener("click", function(event){
-        console.log("document.body.addEventListener----: ",event.toElement.className);
         if(event.toElement.className === 'i18n'){
-            console.log("angular.element(event.toElement.nextElementSibling): ",event.toElement.parentNode.parentNode.nextElementSibling);
             var mdChips = angular.element(event.toElement);
-            console.log("mdChips: ",mdChips);
             var datai18n = mdChips.attr("data-i18n");
-            console.log("datai18n: ",datai18n);
             var inputId = angular.element(event.toElement.parentNode.parentNode.nextElementSibling).find("input").attr("class");
-            console.log("inputId: ",inputId);
             var field = "";
             if(datai18n === 'COMPOSE/TITLE_TO'){
                 field = 'to';
@@ -86,7 +70,6 @@ function addBodyListener(){
 }
 
 if(selectUsersService){
-    console.log("selectUsersServiceselectUsersService");
     addMessageListener();
     addStyles();
     addBodyListener();
